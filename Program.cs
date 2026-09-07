7b9df4b42816fe8d781751fa2f6f2ab2b51db78b
violin69 <mauriciox676@gmail.com>
2026-09-07 10:32:52 -0500

    HOLA, Compañeros

--- Program.cs
+++ Program.cs
@@ -8,8 +8,33 @@ namespace EJERCICIO_SEMANA4
 {
     internal class Program
     {
+        static void Titulo()
+        {
+            Console.WriteLine("=============================================");
+            Console.WriteLine("\t\tBANCO NACIONAL");
+            Console.WriteLine("                   $ ");
+            Console.WriteLine("                   $ ");
+            Console.WriteLine("                   $ ");
+            Console.WriteLine("              $$$$$$$$$$$   ");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("=============================================");
+
+            Console.WriteLine("Todos los asociados al banco tienen de beneficio 1,000.00 Nuevos Soles.");
+        }
+
+
         static void Main(string[] args)
         {
+            // hola
+
+
+        
+
+
+
         }
     }
 }
